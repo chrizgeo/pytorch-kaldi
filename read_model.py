b4363d9b385c94ee2ee38235f1efd824c5ee79f7
CG <christogeorge@live.in>
2021-06-03 18:29:09 +0200

    analyse and print compression of the model

--- read_model.py
+++ read_model.py
@@ -4,9 +4,19 @@ import numpy as np
 import torch
 import sru
 import neural_networks
+import sys
+import os
 
-modelFile = open("exp/TIMIT_SRU_fbank_simple_prune/exp_files/final_architecture1.pkl", 'rb')
+pickle_file = sys.argv[1]
+if not os.path.exists(pickle_file):
+    sys.stderr.write("ERROR: The config file %s does not exist!\n" % (pickle_file))
+    sys.exit(0)
+else:
+    modelFile = open(pickle_file, 'rb')
+
 modelDict = torch.load(modelFile)
+param_count = 0
+zeros_count = 0
 for key in modelDict:
     print(key)
     if key == 'model_par':
@@ -14,9 +24,13 @@ for key in modelDict:
             if 'weight' in innerKey.split("."):
                 print(innerKey)
                 numpy_array = modelDict[key][innerKey].cpu().detach().numpy()
+                param_count += numpy_array.size
                 print(numpy_array.size)
-                print(np.max(numpy_array))
-                print(np.min(numpy_array))
+                #print(np.max(numpy_array))
+                #print(np.min(numpy_array))
+                zeros_count += np.count_nonzero(numpy_array == 0)
                 print(np.count_nonzero(numpy_array == 0))
                 #plt.hist(numpy_array, bins='auto')
                 #plt.show()
+compression = zeros_count/param_count
+print("Compression ratio : %f " %compression)
